b7d17a586a14e872a3fec267af83e80d07bbe721
Elijah Schow <elijah.schow@gmail.com>
2020-03-07 15:09:00 -0600

    Enhancement: add spacing below notif list headers

--- src/components/notification-list.js
+++ src/components/notification-list.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Notification from './notification'
 
 const NotificationList = ({ heading, items, clearMessage }) => (
     <div>
-        <Typography variant="h6">{heading}</Typography>
-        <Typography variant="body1">Count {items.length}</Typography>
+        <Box mb={1}>
+            <Typography variant="h6">{heading}</Typography>
+            <Typography variant="body1">Count {items.length}</Typography>
+        </Box>
         <Grid container direction="column" spacing={2}>
             {items.map(item => (
                 <Grid item key={item.id}>
