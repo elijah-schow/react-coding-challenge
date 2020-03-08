461c78c00e3572cdb53df167ed1252f11b879ef1
Elijah Schow <elijah.schow@gmail.com>
2020-03-07 18:51:46 -0600

    Update error-snackbar.test.js
    - Create functions to generate repeated data
    - Ensure that the message text is displayed
    - Ensure that the onClose handler is called when the close button is clicked
    - Ensure that the onClose handler is called after 2 seconds

--- src/components/__tests__/error-snackbar.test.js
+++ src/components/__tests__/error-snackbar.test.js
@@ -1,19 +1,72 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, getByRole, getByText, fireEvent } from '@testing-library/react'
 import ErrorSnackbar from '../error-snackbar'
 
+const message = () => ({
+    id: "930b1506-699c-57d9-b697-a6b2bbfc63e9",
+    priority: 1,
+    message: "The quick, brown fox jumped over the lazy dog.",
+})
+
+const noop = () => { }
+
 describe('error-snackbar', () => {
+
     it('renders #smoke', () => {
         const props = {
-            item: {
-                id: "930b1506-699c-57d9-b697-a6b2bbfc63e9",
-                priority: 1,
-                message: "The quick, brown fox jumped over the lazy dog.",
-            },
+            item: message(),
             open: true,
-            onClose: () => { },
+            onClose: noop,
         }
         const { container } = render(<ErrorSnackbar {...props} />)
         expect(container).toBeVisible()
     })
+
+    it('displays the message', () => {
+        const item = message()
+        const props = { item, open: true, onClose: noop }
+        const { container } = render(<ErrorSnackbar {...props} />)
+
+        const node = getByText(container, item.message)
+
+        expect(node).toBeDefined()
+    })
+
+    /**
+     * @warning this test assumes that "Close" is the only button inside this
+     * component. Update this test if that assumption becomes invalid.
+     */
+    it('calls the event handler when the close button is clicked', () => {
+        const onClose = jest.fn()
+        const props = {
+            item: message(),
+            open: true,
+            onClose,
+        }
+        const { container } = render(<ErrorSnackbar {...props} />)
+
+        fireEvent.click(getByRole(container, 'button')) // see warning
+
+        expect(onClose).toBeCalled()
+    })
+
+    /**
+     * @see https://jestjs.io/docs/en/timer-mocks
+     */
+    it('closes after 2 seconds', () => {
+        jest.useFakeTimers();
+
+        const onClose = jest.fn()
+        const props = {
+            item: message(),
+            open: true,
+            onClose,
+        }
+        const { container } = render(<ErrorSnackbar {...props} />)
+
+        expect(onClose).not.toBeCalled()
+        jest.advanceTimersByTime(2000)
+        expect(onClose).toBeCalled()
+    })
+
 })
