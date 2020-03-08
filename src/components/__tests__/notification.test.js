1a91ca2f98b9bfa82d4dcdfd9f0973f5428991e7
Elijah Schow <elijah.schow@gmail.com>
2020-03-07 18:14:12 -0600

    Test: add more unit tests for <Notification />
    - Ensure that the notification displays the provided message text
    - Ensure that the event handler is called when the clear button is clicked

--- src/components/__tests__/notification.test.js
+++ src/components/__tests__/notification.test.js
@@ -1,18 +1,51 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent, getByRole, getByText } from '@testing-library/react'
 import Notification from '../notification'
 
+const message = () => ({
+    id: "930b1506-699c-57d9-b697-a6b2bbfc63e9",
+    priority: 1,
+    message: "The quick, brown fox jumped over the lazy dog.",
+})
+
+const noop = () => { }
+
 describe('notification', () => {
+
     it('renders #smoke', () => {
         const props = {
-            item: {
-                id: "930b1506-699c-57d9-b697-a6b2bbfc63e9",
-                priority: 1,
-                message: "The quick, brown fox jumped over the lazy dog.",
-            },
-            clearMessage: () => { }
+            item: message(),
+            clearMessage: noop,
         }
         const { container } = render(<Notification {...props} />)
         expect(container).toBeVisible()
     })
+
+    it('displays the message', () => {
+        const item = message()
+        const props = { item, clearMessage: noop }
+        const { container } = render(<Notification {...props} />)
+
+        const node = getByText(container, item.message)
+
+        expect(node).toBeDefined()
+    })
+
+    /**
+     * @warning this test assumes that "Clear" is the only button inside this
+     * component. Update this test if that assumption becomes invalid.
+     */
+    it('calls the event handler when the clear button is clicked', () => {
+        const clearMessage = jest.fn()
+        const props = {
+            item: message(),
+            clearMessage,
+        }
+        const { container } = render(<Notification {...props} />)
+
+        fireEvent.click(getByRole(container, 'button')) // see warning
+
+        expect(clearMessage).toBeCalled()
+    })
+
 })
